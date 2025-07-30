75eaa03cc9afeda7d40c37164c7ede337b88d28e
saurabh pankaj <rv446382@gmail.com>
2025-07-30 13:06:20 +0530

    fix: Update API base URL for the application

--- DevConnect-frontend/src/api/index.js
+++ DevConnect-frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://dev-connect-api-eta.vercel.app", // Adjust the base URL as needed
+    baseURL: "https://codeconnect-klmd.onrender.com", // Adjust the base URL as needed
 });
 
 // Interceptor to add token to headers
