88848e136198d307bdfa9e40fec8ba294a2e5878
saurabh pankaj <rv446382@gmail.com>
2025-07-30 12:47:36 +0530

    fix: Use environment variable for server port configuration

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
